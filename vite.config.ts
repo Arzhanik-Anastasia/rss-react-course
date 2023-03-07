e067b6df19f1698f06c07c233a489ef083b788fc
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-03-07 08:52:48 +0300

    feat: add init project

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
+import * as vite from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default vite.defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./src/setupTests.ts'],
+  },
+});
