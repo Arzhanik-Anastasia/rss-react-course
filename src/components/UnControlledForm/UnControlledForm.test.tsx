682030f9df708e0562cd555f0ff4d89502e1827c
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-03-24 16:12:36 +0300

    fix: fixed test

--- src/components/UnControlledForm/UnControlledForm.test.tsx
+++ src/components/UnControlledForm/UnControlledForm.test.tsx
@@ -1,46 +1,26 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import CardForm from './../../components/CardForm/CardForm';
 import { vi } from 'vitest';
 import UncontrolledForm from './UnControlledForm';
+import { checkedDate, validateZipCode } from './../../utils/helpers';
 
 describe('Form', () => {
-  it('test firstName Input', async () => {
-    const onAddCard = vi.fn();
-    const user = userEvent.setup();
-    render(<UncontrolledForm onAddCard={onAddCard} />);
-    const inputFirstName = screen.getByTestId('input-firstName');
-    expect(inputFirstName).toBeInTheDocument();
-    expect(screen.queryByText(/Слишком короткое имя/i)).toBeNull();
-    await user.type(inputFirstName, 'te');
-    expect(screen.queryByText(/Слишком короткое имя/i)).toBeInTheDocument();
-    await user.type(inputFirstName, 'Ivan');
-    expect(screen.queryByText(/Слишком короткое имя/i)).toBeNull();
-  });
-
-  it('test lastName Input', async () => {
-    const onAddCard = vi.fn();
-    render(<UncontrolledForm onAddCard={onAddCard} />);
-    const user = userEvent.setup();
-    const inputLastName = screen.getByTestId('input-lastName');
-    expect(inputLastName).toBeInTheDocument();
-    expect(screen.queryByText(/Слишком короткое имя/i)).toBeNull();
-    await user.type(inputLastName, 'te');
-    expect(screen.queryByText(/Слишком короткое имя/i)).toBeInTheDocument();
-    await user.type(inputLastName, 'Ivanov');
-    expect(screen.queryByText(/Слишком короткое имя/i)).toBeNull();
-  });
-
   it('test zipCode Input', async () => {
     const onAddCard = vi.fn();
     const user = userEvent.setup();
     render(<UncontrolledForm onAddCard={onAddCard} />);
+    const button = screen.getByTestId('button-submit-form');
     const inputZipCode = screen.getByTestId('input-zipcode');
     expect(inputZipCode).toBeInTheDocument();
     expect(screen.queryByText(/Минимальное количество символов - 6/i)).toBeNull();
+    await user.click(button);
     await user.type(inputZipCode, '123');
     expect(screen.queryByText(/Минимальное количество символов - 6/i)).toBeInTheDocument();
     await user.type(inputZipCode, '123456');
+    await user.click(button);
     expect(screen.queryByText(/Минимальное количество символов - 6/i)).toBeNull();
+    expect(validateZipCode('123456')).toBe(true);
   });
 
   it('test birthDay Input', async () => {
@@ -50,61 +30,46 @@ describe('Form', () => {
     const inputBirthDay = screen.getByTestId('input-birthDay');
     expect(inputBirthDay).toBeInTheDocument();
     expect(screen.queryByText(/Только пользователи старше 18 лет/i)).toBeNull();
+    const button = screen.getByTestId('button-submit-form');
     await user.type(inputBirthDay, '2022-04-22');
+    await user.click(button);
     expect(screen.queryByText(/Только пользователи старше 18 лет/i)).toBeInTheDocument();
+    expect(checkedDate('02-08-2000')).toBe(true);
   });
 
-  it('test country Select', () => {
-    const onAddCard = vi.fn();
-    render(<UncontrolledForm onAddCard={onAddCard} />);
-    const combobox = screen.getByRole('combobox');
-    expect(combobox).toBeInTheDocument();
-  });
-
-  it('renders all options', () => {
-    const onAddCard = vi.fn();
-    render(<UncontrolledForm onAddCard={onAddCard} />);
-    const selectedOption = screen.getAllByRole('option');
-    expect(selectedOption.length).toEqual(3);
-  });
-
-  it('test avatar Input', async () => {
-    const onAddCard = vi.fn();
-    window.URL.createObjectURL = vi.fn();
-    const user = userEvent.setup();
+  it('renderCard with news true', async () => {
+    window.URL.createObjectURL = vi.fn((file) => (file instanceof File && file.name) || '');
     const file = new File(['hello'], 'hello.png', { type: 'image/png' });
-    render(<UncontrolledForm onAddCard={onAddCard} />);
-    const inputFile = screen.getByTestId('input-avatar');
-    expect(inputFile).toBeInTheDocument();
-    expect(screen.queryByText(/Выберите фото/i)).toBeNull();
-    await user.upload(inputFile, file);
-    await user.upload(inputFile, null as unknown as File);
-    expect(screen.queryByText(/Выберите фото/i)).toBeInTheDocument();
+    const card = {
+      firstName: 'Ivan',
+      lastName: 'Ivanov',
+      zipCode: '123456',
+      birthDay: '12-12-2000',
+      country: 'Belarus',
+      news: true,
+      avatar: file,
+    };
+    const { getAllByRole, queryByText } = render(<CardForm {...card} />);
+    const li = getAllByRole('listitem');
+    expect(li.length).toEqual(1);
+    expect(queryByText(/Получать уведомления: Да/i)).toBeInTheDocument();
   });
 
-  it('test input checkBox switcher', async () => {
-    const onAddCard = vi.fn();
-    render(<UncontrolledForm onAddCard={onAddCard} />);
-    const user = userEvent.setup();
-    const inputSwitcher = screen.getByTestId('input-switcher') as HTMLInputElement;
-    expect(inputSwitcher).toBeInTheDocument();
-    await user.click(inputSwitcher);
-    expect(inputSwitcher.checked).toEqual(true);
-    await user.click(inputSwitcher);
-    expect(inputSwitcher.checked).toEqual(false);
-  });
-
-  it('test input checkBox agree', async () => {
-    const onAddCard = vi.fn();
-    const user = userEvent.setup();
-    render(<UncontrolledForm onAddCard={onAddCard} />);
-    const inputCheckbox = screen.getByTestId('input-checkbox') as HTMLInputElement;
-    expect(inputCheckbox).toBeInTheDocument();
-    expect(screen.queryByText(/Необходимо Ваше согласие/i)).toBeNull();
-    await user.click(inputCheckbox);
-    expect(inputCheckbox.checked).toEqual(true);
-    await user.click(inputCheckbox);
-    expect(inputCheckbox.checked).toEqual(false);
-    expect(screen.queryByText(/Необходимо Ваше согласие/i)).toBeInTheDocument();
+  it('renderCard with news false', async () => {
+    window.URL.createObjectURL = vi.fn((file) => (file instanceof File && file.name) || '');
+    const file = new File(['hello'], 'hello.png', { type: 'image/png' });
+    const card = {
+      firstName: 'Ivan',
+      lastName: 'Ivanov',
+      zipCode: '123456',
+      birthDay: '12-12-2000',
+      country: 'Belarus',
+      news: false,
+      avatar: file,
+    };
+    const { getAllByRole, queryByText } = render(<CardForm {...card} />);
+    const li = getAllByRole('listitem');
+    expect(li.length).toEqual(1);
+    expect(queryByText(/Получать уведомления: Нет/i)).toBeInTheDocument();
   });
 });
--- src/pages/FormPage/FormPage.test.tsx
+++ src/pages/FormPage/FormPage.test.tsx
@@ -1,6 +1,6 @@
-/* eslint-disable @typescript-eslint/no-non-null-assertion */
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import UncontrolledForm from './../../components/UnControlledForm/UnControlledForm';
 
 import { BrowserRouter } from 'react-router-dom';
 import { vi } from 'vitest';
@@ -11,14 +11,13 @@ describe('render Form Page', () => {
     window.URL.createObjectURL = vi.fn();
     const user = userEvent.setup();
     const file = new File(['hello'], 'hello.png', { type: 'image/png' });
-    render(
+    const { container } = render(
       <BrowserRouter>
         <FormPage />
       </BrowserRouter>
     );
     expect(screen.queryByText(/Данные успешно добавлены/i)).toBeNull();
     const button = screen.getByTestId('button-submit-form');
-    expect(button).toBeDisabled();
     const inputFirstName = screen.getByTestId('input-firstName');
     await user.type(inputFirstName, 'Ivan');
     const inputLastName = screen.getByTestId('input-lastName');
@@ -37,41 +36,29 @@ describe('render Form Page', () => {
     await user.click(inputSwitcher);
     const inputCheckbox = screen.getByTestId('input-checkbox') as HTMLInputElement;
     await user.click(inputCheckbox);
-    expect(screen.getByTestId('button-submit-form')).not.toBeDisabled();
-    await user.click(screen.getByTestId('button-submit-form'));
-    const li = screen.getAllByRole('listitem');
-    expect(li.length).toEqual(1);
-    expect(screen.queryByText(/Данные успешно добавлены/i)).toBeInTheDocument();
-    const closeElem = screen.getByTestId('close');
-    expect(closeElem).toBeInTheDocument();
-    await user.click(closeElem);
-    expect(screen.queryByTestId('modal')).toBeNull();
-    expect(screen.getByTestId('button-submit-form')).toBeDisabled();
-    expect(screen.getByTestId('news')).toHaveTextContent('Да');
+    await user.click(button);
+    expect(container);
   });
 
-  it('add Card with news', async () => {
-    window.URL.createObjectURL = vi.fn();
+  it('render Card from form', async () => {
+    const onAddCard = vi.fn();
+    window.URL.createObjectURL = vi.fn((file) => (file instanceof File && file.name) || '');
     const file = new File(['hello'], 'hello.png', { type: 'image/png' });
     const user = userEvent.setup();
-    render(
-      <BrowserRouter>
-        <FormPage />
-      </BrowserRouter>
-    );
-    const inputFirstName = screen.getByTestId('input-firstName');
+    const { getByTestId } = render(<UncontrolledForm onAddCard={onAddCard} />);
+    const inputFirstName = getByTestId('input-firstName');
     await user.type(inputFirstName, 'Ivan');
-    const inputLastName = screen.getByTestId('input-lastName');
+    const inputLastName = getByTestId('input-lastName');
     await user.type(inputLastName, 'Ivanov');
-    const inputZipCode = screen.getByTestId('input-zipcode');
+    const inputZipCode = getByTestId('input-zipcode');
     await user.type(inputZipCode, '123456');
-    const inputBirthDay = screen.getByTestId('input-birthDay');
+    const inputBirthDay = getByTestId('input-birthDay');
     await user.type(inputBirthDay, '2000-04-22');
-    const inputFile = screen.getByTestId('input-avatar') as HTMLInputElement;
+    const inputFile = getByTestId('input-avatar') as HTMLInputElement;
     await user.upload(inputFile, file);
-    const inputCheckbox = screen.getByTestId('input-checkbox') as HTMLInputElement;
+    const inputCheckbox = getByTestId('input-checkbox') as HTMLInputElement;
     await user.click(inputCheckbox);
-    await user.click(screen.getByTestId('button-submit-form'));
-    expect(screen.getByTestId('news')).toHaveTextContent('Нет');
+    await user.click(getByTestId('button-submit-form'));
+    expect(inputFile.files).toHaveLength(1);
   });
 });
