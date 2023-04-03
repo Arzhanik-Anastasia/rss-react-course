8cf44b49778bb7a0b6d7fedc24fd2d227316030a
Arzhanik Anastasia <Anastasia911@mail.ru>
2023-04-03 08:56:21 +0300

    fix: lint error

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,5 +1,4 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import About from './pages/About/About';
